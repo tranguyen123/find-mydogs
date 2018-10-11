fd403d583bcb8613efc2ac7bbb27e88bf782f8c7
tra <nellyng2013@gmail.com>
2018-10-10 20:02:06 -0700

    update

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>GAME BY TRA</p>
+	</header>
+);
+
+export default Footer;
